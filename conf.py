726faf182f9890677923a8f8968dc99e1ce2ca0c
Danelle Cline <dcline@mbari.org>
2017-09-14 16:35:18 -0700

    renamed collection and subset the keys for testing

--- conf.py
+++ conf.py
@@ -1,5 +1,3 @@
-import os
-
 # basic constants
 CLASSES = ['BENTHOCODON', 'ECHINOCREPIS', 'PENIAGONE_SP_1', 'PENIAGONE_SP_2',
            'PENIAGONE_SP_A', 'PENIAGONE_VITRAE', 'SCOTOPLANES_GLOBOSA', 'ELPIDIA']
@@ -14,14 +12,16 @@ ALL_CLASSES = ['ELPIDIA', 'PENIAGONE_PAPILLATA', 'ECHINOCREPIS', 'SCOTOPLANES_GL
 BASE_DIR_RAW = '/Volumes/DeepLearningTests/nyee_datasets/frcnn_data/' 
 
 # name of the data collection
-COLLECTION_NAME = 'MBARI_ROVBENTHIC2017'
+COLLECTION_NAME = 'MBARI_BENTHIC_2017'
 
 # base directory structure to store converted annotations
 BASE_DIR_CONVERT = '/Volumes/DeepLearningTests/training_data_to_export/'
 
-TRAIN_VID_KEYS = ['D008_03HD', 'D0232_04HD', 'D0442_06HD', 'D0443_05HD',
-                  'D0673_04HD', 'D0772_09HD', 'D0904_D3HD']
-TEST_VID_KEYS = ['D0232_03HD', 'D0772_10HD']
+TRAIN_VID_KEYS = ['D008_03HD']
+  #, 'D0232_04HD', 'D0442_06HD', 'D0443_05HD',
+  #                'D0673_04HD', 'D0772_09HD', 'D0904_D3HD']
+TEST_VID_KEYS = ['D0232_03HD']
+#, 'D0772_10HD']
  
 
 # directories for output 
