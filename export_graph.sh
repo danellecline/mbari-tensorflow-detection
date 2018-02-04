9197d355fbce4c042158731ae886df08292811da
danellecline <dcline@mbari.org>
2018-02-03 16:45:11 -0800

    woking graph export

--- export_graph.sh
+++ export_graph.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 # Export graph of traing model 
-# usage: export_graph.sh <model.ckpt> <output_directory>
-# e.g. export_graph.sh faster_rcnn_resnet101_coco_20_smallanchor0 model.ckpt-1924
+# usage: export_graph.sh <modelname> <model.ckpt> 
+# e.g. export_graph.sh faster_rcnn_resnet101_coco_20_smallanchor model.ckpt-1924 
 source ~/Dropbox/GitHub/venv-aesa-tensorflow-detection-devbox/bin/activate
 pushd tensorflow_models/research
 export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim:`pwd`/object_detection
 popd
-python object_detection/export_inference_graph.py \
+OUT_DIR=models/export/"$1"
+mkdir -p $OUDIR 
+# force CPU execution for this
+export CUDA_VISIBLE_DEVICES=""
+python tensorflow_models/research/object_detection/export_inference_graph.py \
     --input_type image_tensor \
     --pipeline_config_path models/"$1"/pipeline_devbox.config \
-    --trained_checkpoint_prefix models/"$2"/"$3" \
-    --output_directory models/"$2"
+    --trained_checkpoint_prefix models/"$1"/train/"$2" \
+    --output_directory $OUT_DIR
