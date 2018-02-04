81e8ada159ed63831f2c1fa1a355d13cfc2a04a7
danellecline <dcline@mbari.org>
2018-02-03 23:24:02 -0800

    reworked run to step through single model array

--- run_all.sh
+++ run_all.sh
@@ -3,43 +3,27 @@ set -x
 function tmux_start_train { tmux new-window -d  -n "train:GPU$1" "${*:2}; exec bash"; }
 function tmux_start_test { tmux new-window -d  -n "test:GPU$1" "${*:2}; exec bash"; }
 
-models=( ["0"]="ssd_inception_v2_coco_600" ["1"]="ssd_inception_v2_coco_300")
+models=(
+["0"]="faster_rcnn_resnet101_coco_600_smallanchor" \
+["1"]="faster_rcnn_resnet101_coco_300_smallanchor" \
+["2"]="faster_rcnn_resnet101_coco_100_smallanchor" \
+["3"]="faster_rcnn_resnet101_coco_50_smallanchor" \
+["4"]="rfcn_resnet101_coco_600_smallanchor" \
+["5"]="rfcn_resnet101_coco_300_smallanchor" \
+["6"]="rfcn_resnet101_coco_100_smallanchor" \
+["7"]="rfcn_resnet101_coco_50_smallanchor")
+
+NUM_MODELS=8
+# Run two models at a time for 3 hours each, splitting testing and training across 4 GPUS
+for i in $(seq 0 2 $(($NUM_MODELS-1))); do
 tmux new-session -d -s "train"
-GPU_IDX=2
-for gpu_index in $(seq 0 $(($GPU_IDX-1))); do
-  tmux_start_train ${gpu_index} \
-  ./run.sh ${models[gpu_index]} train ${gpu_index}
-done
-GPU_IDX=4
-models=( ["2"]="ssd_inception_v2_coco_600" ["3"]="ssd_inception_v2_coco_300")
+  tmux_start_train 0 ./run.sh ${models[i]} train 0 > test0.txt
+  tmux_start_train 1 ./run.sh ${models[i + 1]} train 1 > test1.txt
+
 tmux new-session -d -s "test"
-for gpu_index in $(seq 2 $(($GPU_IDX-1))); do
-  tmux_start_test ${gpu_index} \
-  ./run.sh ${models[gpu_index]} test ${gpu_index}
-done
-sleep 11h
-tmux kill-session -t train
-tmux kill-session -t test
+  tmux_start_test 2 ./run.sh ${models[i]} test 2 > test2.txt
+  tmux_start_test 3 ./run.sh ${models[i + 1]} test 3 > test3.txt
 sleep 5s
-exit
-
-# Run models for 6 hours each, splitting testing and training across 4 GPUS
-GPU_IDX=2
-models=( ["0"]="faster_rcnn_resnet101_coco_300" ["1"]="faster_rcnn_resnet101_coco_100")
-tmux new-session -d -s "train"
-for gpu_index in $(seq 0 $(($GPU_IDX-1))); do
-  tmux_start_train ${gpu_index} \
-  ./run.sh ${models[gpu_index]} train ${gpu_index} 
-done
-GPU_IDX=4
-models=( ["2"]="faster_rcnn_resnet101_coco_300" ["3"]="faster_rcnn_resnet101_coco_100")
-tmux new-session -d -s "test"
-for gpu_index in $(seq 2 $(($GPU_IDX-1))); do
-  tmux_start_test ${gpu_index} \
-  ./run.sh ${models[gpu_index]} test ${gpu_index} 
-done
-sleep 6h
 tmux kill-session -t train
 tmux kill-session -t test
-
-# allow few seconds for everything to die
+done
