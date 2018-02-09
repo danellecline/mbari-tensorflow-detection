c8cb1f092f7db7134f1535a373844f2a68a228f1
danellecline <dcline@mbari.org>
2018-02-09 14:37:07 -0800

    added support for CPU and unique inference records file

--- run_inference.sh
+++ run_inference.sh
@@ -7,16 +7,23 @@ pushd tensorflow_models/research
 export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim:`pwd`/object_detection
 popd
 export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64/
-export CUDA_VISIBLE_DEVICES="" #"$3"
+if [ "$3" == -1 ]
+then
+export CUDA_VISIBLE_DEVICES=""
+CSV_FILE=`pwd`/data/"$1"-"$2"-CPU-time.csv
+else
+export CUDA_VISIBLE_DEVICES="$3"
+CSV_FILE=`pwd`/data/"$1"-"$2"-GPU-time.csv
+fi
 
-TF_RECORD_FILE=`pwd`/data/"$1"
-MODEL_DIR=`pwd`//models/export/"$2"/frozen_inference_graph.pb
-CSV_FILE=`pwd`/data/"$1"-"$2"-GPUtime.csv
+TF_RECORD_FILE_OUT=`pwd`/data/"$1"-"$2"_detections_images.record
+TF_RECORD_FILE_IN=`pwd`/data/"$1"
+MODEL_DIR=`pwd`/models/export/"$2"/frozen_inference_graph.pb
 
 python tensorflow_models/research/object_detection/inference/infer_detections.py \
-  --input_tfrecord_paths=$TF_RECORD_FILE \
-  --output_tfrecord_path=$TF_RECORD_FILE\.detections.record \
+  --input_tfrecord_paths=$TF_RECORD_FILE_IN \
+  --output_tfrecord_path=$TF_RECORD_FILE_OUT \
   --csv_path=$CSV_FILE \
-  --inference_graph=$MODEL_DIR
-  
-#--discard_image_pixels
+  --inference_graph=$MODEL_DIR 
+#\
+#  --discard_image_pixels
