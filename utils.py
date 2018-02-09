a01da90ceb2b0c776aeee2db4a826f1150c6b366
Danelle Cline <dcline@mbari.org>
2018-02-09 15:48:49 -0800

    Split isn't working on array

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import os
 import glob
 import random
 
+
 def ensure_dir(d):
   """
   ensures a directory exists; creates it if it does not
@@ -21,29 +22,25 @@ def split(collection_dir, train_per, test_per):
   :param test_per:
   :return:
   '''
-
   annotations = []
   for xml_in in glob.iglob(collection_dir + '/**/*.xml', recursive=True):
-      print('Found {0}'.format(xml_in))
-      path, filename = os.path.split(xml_in)
-      annotations.append(filename)
+    print('Found {0}'.format(xml_in))
+    path, filename = os.path.split(xml_in)
+    annotations.append(filename)
 
   print('Found {0} xml annotations in {1}'.format(len(annotations), collection_dir))
 
   # split randomly
   random.shuffle(annotations)
-
   total = len(annotations)
-  train_data = annotations[:round(train_per * total)]
-  test_data = annotations[:round(test_per * total)]
-
   ensure_dir(collection_dir)
+
   with open(os.path.join(collection_dir, 'train.txt'), 'w') as f:
-      for l in train_data:
-          path, filename = os.path.split(l)
-          f.write(filename + '\n')
+    for i in range(round(train_per * total)):
+      path, filename = os.path.split(annotations[i])
+      f.write(filename + '\n')
 
   with open(os.path.join(collection_dir, 'test.txt'), 'w') as f:
-      for l in test_data:
-          path, filename = os.path.split(l)
-          f.write(filename + '\n')
+    for i in range(round(test_per * total)):
+      path, filename = os.path.split(annotations[-1 * i])
+      f.write(filename + '\n')
