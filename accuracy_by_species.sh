27b9a7f7a1915987826c8261aab649e5f4235ce8
danellecline <dcline@mbari.org>
2018-02-15 13:55:53 -0800

    removed unused scripts

--- accuracy_by_species.sh
+++ accuracy_by_species.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 BASE_DIR=~/Dropbox/GitHub/mbari-tensorflow-detection 
-source ~/Dropbox/GitHub/venv-aesa-tensorflow-detection/bin/activate
+source ~/Dropbox/GitHub/venv-aesa-tensorflow-detection-devbox/bin/activate
 python accuracy_by_species.py
+scp mAPPENIAGONE_SP_A.png  dcline@mbari1913:~/Dropbox/poster/ 
+scp mAPECHINOCREPIS.png  dcline@mbari1913:~/Dropbox/poster/
+scp mAPSCOTOPLANES_GLOBOSA.png dcline@mbari1913:~/Dropbox/poster/
+scp mAPBENTHOCODON.png dcline@mbari1913:~/Dropbox/poster/ 
--- box_proposal_plot.sh
+++ box_proposal_plot.sh
@@ -2,3 +2,4 @@
 BASE_DIR=~/Dropbox/GitHub/mbari-tensorflow-detection 
 source ~/Dropbox/GitHub/venv-aesa-tensorflow-detection-devbox/bin/activate
 python box_proposal_plot.py
+scp mAP.png  dcline@mbari1913:'~/Dropbox/poster/mAP_MBARI_BENTHIC_ALL.png' 
